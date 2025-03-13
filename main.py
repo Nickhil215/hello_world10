baa0c273410242db19c5d46f12ec10f5c172684b
Nikhil Vanamala <105160327+Nickhil215@users.noreply.github.com>
2025-03-14 00:40:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,42 @@
 
 
 
+# from flask import Flask, request, jsonify
+
+# app = Flask(__name__)
+
+# def function_one():
+#     return "Hello from Function One!"
+
+# def function_two():
+#     return "Hello from Function Two!"
+
+# def function_three():
+#     return "Hello from Function Three!"
+
+# @app.route('/invoke', methods=['POST'])
+# def dispatcher():
+#     data = request.get_json()
+#     function_name = data.get("function")
+
+#     functions = {
+#         "function_one": function_one,
+#         "function_two": function_two,
+#         "function_three": function_three,
+#     }
+
+#     if function_name in functions:
+#         return jsonify({"result": functions[function_name]()})
+#     else:
+#         return jsonify({"error": "Function not found"}), 404
+
+# if __name__ == "__main__":
+#     app.run(port=8080)
+
+
+
+# ==============================================================
+
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -59,5 +95,12 @@ def dispatcher():
     else:
         return jsonify({"error": "Function not found"}), 404
 
+@app.route('/one', methods=['GET'])
+def call_function_one():
+    
+    return jsonify({"result":function_one})()
+   
+
 if __name__ == "__main__":
     app.run(port=8080)
+
